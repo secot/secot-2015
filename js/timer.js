eb1da65492db2167bb62eecb0537856afafe99b4
Andre Bonfatti <andrebnf@gmail.com>
2015-04-12 22:16:17 -0300

    added modal message for mini-cv & catarse campaign added too

--- js/timer.js
+++ js/timer.js
@@ -13,4 +13,45 @@ window.onload = function(){
 
 		for(var r in a)document.getElementById(r).innerHTML = a[r];setTimeout(e,1e3)
 	};e()
+
+	var g = function(){
+		var l = document.getElementsByClassName("mini-cv");
+
+		for(var i = 0; i < l.length; i++){
+
+			if (l[i].innerHTML.length > 360) {
+				n = document.createElement('span');
+				n.setAttribute("class","expand-mini-cv");
+				n.setAttribute("data-ref",i);
+				n.setAttribute("data-title",l[i].previousSibling.previousSibling.innerHTML);
+				n.setAttribute("data-text",l[i].innerHTML);
+				n.innerHTML = "ver tudo"
+
+				l[i].innerHTML = l[i].innerHTML.substring(0, 440);
+				l[i].innerHTML += "... ";
+
+				n.setAttribute("onclick","guestModal(this)");
+
+				l[i].appendChild(n);
+			};
+		}
+	};g()
 };
+
+function guestModal(e)
+{
+	var m = document.getElementById("modal");
+	var tit = m.childNodes[1].childNodes[1],
+			tex = m.childNodes[1].childNodes[3];
+
+	tit.innerHTML = e.getAttribute("data-title");
+	tex.innerHTML = e.getAttribute("data-text");
+
+	m.style.display = "block";
+};
+
+function closeModal()
+{
+	document.getElementById("modal").style.display = "none";
+
+}
